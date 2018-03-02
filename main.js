463dcec5930296ea8ec6f600d4c6c4379171d8da
Bum Mook Oh <bmo5@uw.edu>
2018-03-01 23:59:18 -0800

    updated html to display home button, and make js to load data only once, major speed improvment.

--- main.js
+++ main.js
@@ -1,56 +1,57 @@
-console.log("hi");
 let rootDomain = "https://s3-us-west-2.amazonaws.com/escience.washington.edu.viziometrics/artstor/deep_mapping/128d/";
 let exampleFile = "drescher_2417f_post_as_8b_srgb.png";
+var global_json;
 
 function getDataByFileName(fileName) {
     var similarDiv = $("#similarImages");
     similarDiv.empty();
 
+    file = global_json[fileName];
+    $("#selectedImg").attr('src', rootDomain + file["img_name"]);
+    $("#title").text(file["Title"]);
+    $("#creator").text(file["Creator"]);
+    for (var i = 1; i <= 10; i++) {
+        var similarFile = file['r' + i];
+        similarDiv.append(createImgByFileName(similarFile, false));
+    }
+}
+
+function loadJson() {
     $.getJSON('./data.json', function (json) {
-        // console.log(json[fileName]);
-        file = json[fileName];
-        $("#selectedImg").attr('src', rootDomain + file["img_name"]);
-        $("#title").text(file["Title"]);
-        $("#creator").text(file["Creator"]);
-        for(var i = 1 ; i <= 10; i++){
-            var similarFile = file['r' + i];
-            similarDiv.append(createImgByFileName(similarFile, false));
-        }
+        global_json = json;
+        getRandomByFileName();
     });
 }
 
-function getRandomByFileName() {
+function getRandomByFileName(){
     var randomDiv = $("#randomImg");
-
-    $.getJSON('./data.json', function (json) {
-        // console.log(json[fileName]);
-        var keys = Object.keys(json);
-
-       
-
-        for(var i = 1 ; i <= 30; i++){
-            var randomImg =  keys[Math.floor(keys.length * Math.random())];
-            randomDiv.append(createImgByFileName(randomImg, true));
-        }
-    });
+    var keys = Object.keys(global_json);
+    for (var i = 1; i <= 30; i++) {
+        var randomImg = keys[Math.floor(keys.length * Math.random())];
+        randomDiv.append(createImgByFileName(randomImg, true));
+    }
 }
 
-function createImgByFileName(fileName, hide){
+function createImgByFileName(fileName, hide) {
     var img = document.createElement('img');
     img.src = rootDomain + fileName;
     img.title = fileName;
     img.classList.add("p-2");
-    img.addEventListener("click", function(){
+    img.addEventListener("click", function () {
         getDataByFileName(fileName);
     });
-    if(hide){
-        img.addEventListener("click", function(){
-            $("#randomImg").hide();
+    if (hide) {
+        img.addEventListener("click", function () {
+            $("#entry").hide();
             $("#search").removeClass("d-none");
         });
     }
     return img;
 }
 
-// getDataByFileName(exampleFile)
-getRandomByFileName();
+document.getElementById("homeBtn").addEventListener("click", function () {
+    $("#entry").show();
+    $("#search").hide();
+});
+
+loadJson();
